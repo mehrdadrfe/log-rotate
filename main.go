4b4ca124b61f8d3cdc713863f1c90839cb273dda
Mehrdad Rafiee <mehrdadrfe@gmail.com>
2025-07-14 18:16:38 +0330

    refactor(golang-flag): added golang flag feat

--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
-	configs := []Config{
-		{
-			NameDir:       "elk",
-			LogDir:        "/var/log/elk",
-			RetentionDays: 10,
-		},
-		{
-			NameDir:       "elk2",
-			LogDir:        "/var/log/elk2",
-			RetentionDays: 20,
-		},
-		{
-			NameDir:       "nginx",
-			LogDir:        "/var/log/nginx",
-			RetentionDays: 5,
-		},
+	name := flag.String("name", "cli", "Name of the directory (for logging)")
+	dir := flag.String("dir", "/var/log/nginx", "Directory to clean")
+	days := flag.Int("days", 7, "Retention period in days")
+
+	flag.Parse()
+
+	fmt.Printf("Running log-rotate: name=%s, dir=%s, retention=%d days\n", *name, *dir, *days)
+
+	config := Config{
+		NameDir:       *name,
+		LogDir:        *dir,
+		RetentionDays: *days,
 	}
 
-	ConcurrentFilesToDelete(configs)
+	ConcurrentFilesToDelete([]Config{config})
 }
